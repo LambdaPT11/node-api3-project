62bebcb5bf8be356f93b45d199fd64e90d666fa7
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-03-18 21:38:34 -0400

    mvp, need to verify post

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,8 +4,8 @@ const logger = (req, res, next) => {
     const method = req.method
     const endPoint = req.originalUrl
     const date = new Date()
-    console.dir(`${method} request to ${endPoint} at ${date}`)
-    next()
+    console.dir(`${method} request to ${endPoint} at ${date}`);
+    next();
 }
 
 const validateUserId = (req, res, next) => {
@@ -15,7 +15,7 @@ const validateUserId = (req, res, next) => {
             if (user) {
                 req.user
             } else {
-                res.status(400).json({ msg: 'invalid user id' })
+                res.status(400).json({ msg: 'invalid user id' });
             }
         })
         next();
@@ -23,16 +23,26 @@ const validateUserId = (req, res, next) => {
 
 const validateUser = (req, res, next) => {
     if (Object.entries(req.body).length === 0) {
-        res.status(400).json({ msg: 'missing user data' })
+        res.status(400).json({ msg: 'missing user data' });
     } else if (!req.body.name) {
-        res.status(400).json({ msg: 'missing required name field' })
+        res.status(400).json({ msg: 'missing required name field' });
     }
     next();
-} 
+}
 
+const validatePost = (req, res, next) => {
+    const { text } = req.body
+    if (Object.entries(req.body).length === 0) {
+        res.status(400).json({ message: 'No User Data' });
+    } else if (!text) {
+        res.status(400).json({ message: 'Missing required text field' });
+    }
+    next();
+}
 
 module.exports = {
     logger,
     validateUser,
     validateUserId,
+    validatePost
 };
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const middleware = require('../middleware/middleware.js');
 const userDB = require('./userDb.js');
+const postsDB = require('../posts/postDb')
 
 const router = express.Router();
 const validateUser = middleware.validateUser
 const validateUserId = middleware.validateUserId
+const validatePost = middleware.validatePost
 
 router.get('/', (req, res) => {
   
@@ -33,20 +35,69 @@ router.post('/', validateUser, (req, res) => {
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
-  // do your magic!
+  const { id } = req.params
+  userDB.getById(id)
+    .then(user => {
+      if (user) {
+        userDB.remove(id)
+      .then(deleted => {
+        if(deleted) {
+          res.status(200).json({ msg: `user ${id} was deleted!\n ${user.name} is dead` });
+        } else { null }
+      })
+      } else { null }
+    })
+    .catch(err => {
+      res.status(500).json({ msg: 'ok, that did not work, like at all. Nope.' })
+    })
 });
 
 router.put('/:id', validateUserId, (req, res) => {
-  // do your magic!
+  const { id } = req.params
+
+  usersDB.update(id, req.body)
+		.then(user => {
+			res.status(200).json({ success: 'Info Updated!', info: req.body })
+		})
+		.catch(err => {
+			res.status(500).json({ error: 'You found me but I cannot provide any info, try again!', err })
+		})
 });
 
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+  const { text } = req.body
+	const user_id = req.params.id
 
-router.post('/:id/posts', validateUserId, (req, res) => {
-  // do your magic!
+	usersDB.getById(user_id)
+		.then(post => {
+			if (!post) {
+				null
+			} else {
+				let newPost = {
+					text,
+					user_id,
+				}
+
+				postsDB.insert(newPost).then(post => {
+					res.status(201).json({ success: post })
+				})
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ error: 'You found me but I cannot provide any info, try again!', err })
+		})
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
-  // do your magic!
+  const { id } = req.params
+
+	usersDB.getUserPosts(id)
+		.then(data => {
+			data ? res.status(200).json(data) : null
+		})
+		.catch(err => {
+			res.status(500).json({ error: 'You found me but I cannot provide any info, try again!', err })
+		})
 });
 
 
